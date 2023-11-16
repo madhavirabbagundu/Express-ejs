0c7d1263aa41a1b96bc4a18b7f1f547a4cf6c4f1
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-11-16 23:59:37 +0530

    work on form using the express ejs

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+const NameData = [];
+const NumberData = [];
+const EmailData = [];
+
+
+app.get('/',(req,res)=>{
+    res.render("app",{Name: NameData, Number:NumberData,Email:EmailData })
+})
+
+
+app.post('/',(req,res)=>{
+    let named = req.body.named;
+    let number = req.body.number;
+    let email = req.body.email;
+
+    NameData.push(named)
+    NumberData.push(number)
+    EmailData.push(email)
+    res.redirect('/')
+})
+
+
+app.listen(4000,()=> console.log("server is running ...."))
